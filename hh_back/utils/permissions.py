aef6e807ebef72a44f7a7b43225635de22083bd8
Atikunchik <47661076+Atikunchik@users.noreply.github.com>
2022-04-19 16:13:16 +0600

    Delete hh_back directory

--- hh_back/utils/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class CompanyPermission(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        if view.action == 'vacancies':
-            return bool(request.user and request.user.is_authenticated)
-        return True
